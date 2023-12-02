e6af98cf90ec56a5723c8d8fac60d0c4804299ef
soumya Hegde <soumyahegde652@gmail.com>
2023-12-02 20:30:32 +0530

    demo of alerts and prompts

---
+++ alertss.js
@@ -0,0 +1,8 @@
+// alert("eror 504");
+let firstname=prompt("enter your first name:");
+let lastname=prompt("enter the last name:")
+let result="welcome " + firstname + " " + lastname;
+
+alert(result);
+
+
