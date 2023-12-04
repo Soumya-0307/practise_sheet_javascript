421fb0c64ae646e933e540120eb6c425c478f950
soumya Hegde <soumyahegde652@gmail.com>
2023-12-04 17:18:57 +0530

    javascript methods

---
+++ exercise.js
@@ -0,0 +1,23 @@
+let msg="     soumya   ";
+// newmsg=msg.trim();
+// alert(newmsg);
+
+alert(msg.toUpperCase().trim());
+
+
+
+let msgs="apnacollegeclass";
+
+console.log(msgs.slice(2,6));
+
+let str="I love codding";
+newstr=str.replace("love","do");
+console.log(newstr);
+
+let word="Amogha";
+
+console.log(word.repeat(3));
+
+
+
+
