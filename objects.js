976449f8c4a5743d160a43f3bdcb09435ebdd5db
soumya Hegde <soumyahegde652@gmail.com>
2023-12-05 23:15:48 +0530

    javascript object demo

---
+++ objects.js
@@ -0,0 +1,65 @@
+let student={
+    name:"Soumya",
+    register:23456,
+    adress:"3rd block",
+
+};
+
+const item={
+    price:100.90,
+    discount:50,
+    colors:["red","pink"]
+};
+
+
+console.log(item["discount"]);
+
+console.log(item.discount);
+
+obj={
+    1:"soumya",
+    3:"swathi",
+    true:"gcsgfasfsdybasjd",
+    undefined:"hxjhasyffxvhagsv"
+};
+
+const students={
+    name:"soumya",
+    age:23,
+    marks:98.0,
+    city:"delhi"
+};
+
+
+ classInfos={
+    amogha:{
+        grade:8.9,
+        city:"bangalore"
+},
+    soumya:{
+        grade:78,
+        city:"mysore"
+    },
+    Swtahi:{
+        grade:90,
+        city:"kumta"
+}
+}
+
+
+const demo=[
+    { 
+        name:"aman",
+        grade:"A+" 
+    },
+    { 
+        name:"amogha",
+        grade:"A" 
+     },
+    { 
+        name:"amrutha",
+        grade:"b+" 
+     }
+]
+
+
