883dad1ef9d3e4bcfb838a6c1f00f70ad12e8a44
soumya Hegde <soumyahegde652@gmail.com>
2023-12-08 11:03:10 +0530

    practise programs

---
+++ practisee.js
@@ -0,0 +1,25 @@
+// let btn=document.querySelector("button");
+let form=document.querySelector("form");
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    alert("form was submitted");
+});
+
+
+
+
+
+// btn.addEventListener("click",function(){
+//     console.log(event);
+//     console.log("button clicked");    
+// });
+
+// btn.addEventListener("dblclick",function(){
+//     console.log(event);
+//     console.log("button clicked");    
+// });
+
+
+
+
