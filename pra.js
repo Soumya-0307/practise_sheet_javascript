5c9fd9905d20e9745fa606d73177ad40b448f4d7
soumya Hegde <soumyahegde652@gmail.com>
2023-12-08 11:02:13 +0530

    practise programs

---
+++ pra.js
@@ -0,0 +1,15 @@
+function traffic(color){
+if(color.value=='red'){
+    console.log("please wait until the light turns to green.");
+}
+
+else if(color.value=='yellow'){
+    console.log("please be ready for the ride.");
+}
+
+else if(color.value=='green'){
+    console.log("go forhead.");
+}
+else
+console.log("not a valid color");
+}
