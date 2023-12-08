eb6b766fa16d6a797e19f2dd5a492457f1ca5ba1
soumya Hegde <soumyahegde652@gmail.com>
2023-12-08 11:00:07 +0530

    programs in javascript

---
+++ p2.js
@@ -0,0 +1,22 @@
+// let price;
+function popcorn(size){
+    if( size.value=='XL')
+    {
+        console.log("your popcorn prize is RS.250");
+    }
+
+    else if( size.value=='L')
+    {
+        console.log("your popcorn prize is RS.200");
+    }
+
+    else if( size.value=='M')
+    {
+        console.log("your popcorn prize is RS.100");
+    }
+
+    else if( size.value=='S')
+    {
+        console.log("your popcorn prize is RS.50");
+    }
+}
